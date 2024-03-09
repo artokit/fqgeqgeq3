8d54e3b0f2db0f33443ba1a0e48eb3b54bfcf788
xr7a <ivandesyatov3@gmail.com>
2024-03-09 22:17:43 +0300

    product repos

--- WebApplication9/Program.cs
+++ WebApplication9/Program.cs
@@ -1,14 +1,16 @@
 using System.Reflection;
 using FluentMigrator;
 using FluentMigrator.Runner;
+using WebApplication9.Domain;
 
 var builder = WebApplication.CreateBuilder(args);
-string connectionString = builder.Configuration.GetConnectionString("Database");
-
+var connectionString = builder.Configuration.GetConnectionString("Database");
+builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<ConnectionDatabase>(_ => new ConnectionDatabase(connectionString));
+builder.Services.AddFluentMigratorCore().ConfigureRunner(rb => rb.AddPostgres().WithGlobalConnectionString(connectionString).ScanIn(Assembly.GetExecutingAssembly()).For.Migrations()).AddLogging(rb => rb.AddFluentMigratorConsole()).BuildServiceProvider(false);
 
 var app = builder.Build();
-builder.Services.AddFluentMigratorCore().ConfigureRunner(rb => rb.AddPostgres().WithGlobalConnectionString(connectionString).ScanIn(Assembly.GetExecutingAssembly()).For.Migrations()).AddLogging(rb => rb.AddFluentMigratorConsole()).BuildServiceProvider(false);
 
 app.UseSwagger();
 app.UseSwaggerUI();
---
+++ WebApplication9/Repositories/ProductRepository.cs
@@ -0,0 +1,59 @@
+using System.Data.Common;
+using Dapper;
+using WebApplication9.Domain;
+using WebApplication9.DTO;
+using WebApplication9.Migrations;
+using WebApplication9.Repositories.Interfaces;
+
+namespace WebApplication9.Repositories;
+
+public class ProductRepository: IProductRepository
+{
+    private ConnectionDatabase _connection;
+
+    public ProductRepository(ConnectionDatabase connection)
+    {
+        _connection = connection;
+    }
+
+    public async Task<List<Product>> GetAll()
+    {
+        using (var connection = _connection.CreateConnection())
+        {
+            return (await connection.QueryAsync<Product>("SELECT * FROM products")).ToList();
+        }
+    }
+
+    public async Task<Product?> GetProduct(int id)
+    {
+        using (var connection = _connection.CreateConnection())
+        {
+            return await connection.QueryFirstOrDefaultAsync<Product>("SELECT * FROM product WHERE id = @id");
+        }
+    }
+
+    public async void Add(ProductDTO product)
+    {
+        using (var connection = _connection.CreateConnection())
+        {
+            await connection.QueryAsync(
+                "INSERT INTO products (name, description, category, price) VALUES (@name, @description, @category, @price)");
+        }
+    }
+
+    public async void Update(int id, ProductDTO productDto)
+    {
+        using (var connection = _connection.CreateConnection())
+        {
+            await connection.QueryAsync("UPDATE products SET name = @name, description = @description, category = @category, price = @price WHERE id = @id");
+        }
+    }
+
+    public async void Delete(int id)
+    {
+        using (var connection = _connection.CreateConnection())
+        {
+            await connection.QueryAsync("DELETE FROM products WHERE id = @id", new { id });
+        }
+    }
+}
